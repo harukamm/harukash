ef8f4c28bdd8abf5f3ba6494d522fd0de081536e
Haruka Matsumoto <harukam0416@gmail.com>
2019-05-25 14:46:34 +0900

    Implemet builtin echo

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 void print_prompt() {
   cout << "^_^ > ";
+}
+
+vector<string> split(const string& s, char sep) {
+  vector<string> elems;
+  stringstream ss(s);
+  string item;
+  while (getline(ss, item, sep)) {
+    if (!item.empty()) {
+      elems.push_back(item);
+    }
+  }
+  return elems;
+}
+
+vector<string> parse(string& s) {
+  return split(s, ' ');
+}
+
+void builtin_echo(const vector<string>& tokens) {
+  for (int i = 1; i < tokens.size(); i++) {
+    cout << tokens[i];
+    if (i + 1 != tokens.size()) {
+      cout << " ";
+    }
+  }
+  cout << endl;
+}
+
+bool handle_builtin(const vector<string>& tokens) {
+  if (tokens.size() == 0) {
+    return false;
+  }
+  const string& first = tokens.at(0);
+  if (first == "echo") {
+    builtin_echo(tokens);
+    return true;
+  }
+  return false;
 }
 
 int main() {
@@ -14,6 +54,15 @@ int main() {
 
     string s;
     getline(cin, s);
+    const vector<string>& tokens = parse(s);
+
+    if (tokens.size() == 0) {
+      continue;
+    }
+    if (handle_builtin(tokens)) {
+      continue;
+    }
+
     cout << s << endl;
   }
 }
