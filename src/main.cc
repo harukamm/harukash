f74c52e6124d6063308cd8244eb095d283a5ee1c
Haruka Matsumoto <harukam0416@gmail.com>
2019-05-25 21:02:41 +0900

    Override '<<' to print vectors

--- src/main.cc
+++ src/main.cc
@@ -25,6 +25,20 @@ struct parsed_obj {
   map<string, int> fdmap;
 };
 
+template<typename T>
+ostream& operator<< (ostream& out, const vector<T>& v) {
+  out << "{";
+  size_t last = v.size() - 1;
+  for(size_t i = 0; i < v.size(); ++i) {
+    out << v[i];
+    if (i != last) {
+      out << ", ";
+    }
+  }
+  out << "}";
+  return out;
+}
+
 void print_prompt() {
   cout << "^_^ > ";
 }
